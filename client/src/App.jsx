d376ac175e98a33c0f7fc71f58a0f433823d7c25
ERIC MATUTU <ericmatutu125@mail.com>
2025-07-01 04:14:36 +0300

    user authentication setup login, signin, verify token

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,9 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-
+/**pages */
 import Home from './pages/Home';
+import Signin from './pages/Signin';
+import Login from './pages/Login';
+
 
 export default function App() {
 
@@ -8,6 +11,8 @@ export default function App() {
   return (
     <Router>
       <Routes>
+        <Route path='/login' element={<Login />}/>
+        <Route path='/signin' element={<Signin />}/>
         <Route path='/' element={<Home />}/>
       </Routes>
     </Router>
