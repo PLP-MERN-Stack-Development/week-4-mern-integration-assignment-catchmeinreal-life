1752dce10f015a71b117e067145d3f3c0010ea76
ERIC MATUTU <ericmatutu125@mail.com>
2025-07-19 02:02:53 +0300

    mongoose Atlas connection: success

--- server/config/database.js
+++ server/config/database.js
@@ -1,15 +1,14 @@
+const mongoose = require('mongoose');
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://ericmatutu125:7jq6qM%40125@nodetutorial.9cc3x.mongodb.net/blogDB?retryWrites=true&w=majority&appName=nodetutorial";
-
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
+connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI)
+    console.log('Connected to MongoDB Atlas')
+  } catch (error) {
+    console.error('Error Connecting to MongoDB Atlas', error.message);
+    process.exit(1);
   }
-});
+}
 
 
-module.exports = client;
+module.exports = connectDB;
